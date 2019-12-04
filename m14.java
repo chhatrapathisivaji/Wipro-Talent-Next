978e3de9c6d22b42ffd2e4ed8ac04d10ae53f357
chhatrapathisivaji <58470174+chhatrapathisivaji@users.noreply.github.com>
2019-12-04 17:05:40 +0530

    Update m14.java

--- m14.java
+++ m14.java
@@ -7,7 +7,7 @@ if(a.length==1)
 int n=Integer.parseInt(a[0]),count=0;
 if(n==1||n==0)
 {
-System.out.println(n+" is neither Prime nor Composite");
+System.out.println(n+" is neither prime nor composite");
 }
 else
 {
@@ -19,11 +19,11 @@ count++;
 if(count==0)
 System.out.println(n+" Is Prime");
 else
-System.out.println(n+" Is Composite");
+System.out.println(n+" Is not a Prime");
 }
 }
 else
-System.out.println("Please Enter a number");
+System.out.println("Please enter an integer number");
 
 }
 }
